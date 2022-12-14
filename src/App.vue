6f07b722dbc43702146cffb4f5c239e435012b15
MrFootwork <pandau.ting@gmail.com>
2022-12-14 22:34:28 +0100

    dropdown toggle hidden and visible

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-// import SelectorStyle from '@/components/SelectorStyle.vue'
 import SelectorStyle from '@/components/SelectorStyle.vue'
 </script>
 
--- src/components/SelectorStyle.vue
+++ src/components/SelectorStyle.vue
@@ -1,13 +1,44 @@
-<template>
-  <div>
-    <h1>jetzt wieder</h1>
-  </div>
-</template>
-
 <script setup lang="ts">
+
+const styles = ['style 1', 'style 2']
 
 </script>
 
-<style scoped lang="scss">
+<template>
+  <div class="dropdown">
+    <label class="style-label" for="dropdown">Styles</label>
+    <ul class="style-container">
+      <li class="style-item" v-for="style in styles">
+        <button>{{ style }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
 
+<style scoped lang="scss">
+.dropdown {
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  & .style-label {
+    color: violet;
+
+    &:hover {
+      color: green;
+    }
+  }
+
+  & .style-container {
+    visibility: hidden;
+    display: none;
+
+
+  }
+
+  .style-label:hover+.style-container {
+    visibility: visible;
+    display: block;
+  }
+}
 </style>
