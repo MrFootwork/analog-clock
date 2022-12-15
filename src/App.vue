e2f4a1f2a7938176d56666018b1c2ddbfc3b77ea
MrFootwork <pandau.ting@gmail.com>
2022-12-16 00:11:09 +0100

    code structure components, scss, file structure

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import SelectorStyle from '@/components/SelectorStyle.vue'
+import ClockWrapper from '@/components/ClockWrapper.vue'
+import ClockFrame from '@/components/ClockFrame.vue'
+import ClockDial from '@/components/ClockDial.vue'
+import ClockHands from '@/components/ClockHands.vue'
 </script>
 
 <template>
 
   <SelectorStyle />
 
-  <div class="wrapper">
-    <div class="dial">
+  <ClockWrapper>
 
-    </div>
-  </div>
+    <Template #frame>
+      <ClockFrame />
+    </Template>
+
+    <Template #dial>
+      <ClockDial />
+    </Template>
+
+    <Template #hands>
+      <ClockHands />
+    </Template>
+
+  </ClockWrapper>
 
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 
 </style>
---
+++ src/components/ClockWrapper.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <slot name="frame"></slot>
+    <slot name="dial"></slot>
+    <slot name="hands"></slot>
+
+    <img src="public/bg__btm-right.svg" alt="">
+    <img src="public/bg__left.svg" alt="">
+    <img src="public/bg__top-right.svg" alt="">
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  /* FIXME add mixins for theming */
+}
+</style>
